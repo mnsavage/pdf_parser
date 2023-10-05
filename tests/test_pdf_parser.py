255f518ca20e0288f329733feeabacee0051add8
themnsavage <mnsavage@crimson.ua.edu>
2023-10-05 15:57:50 -0500

    refactor: run linter

--- tests/test_pdf_parser.py
+++ tests/test_pdf_parser.py
@@ -8,12 +8,11 @@ def test_init():
     parser = Pdf_Parser(file_name)
 
     assert file_name == parser._file_name
-    
+
+
 def test_get_file_name():
     file_name = "file/path/file.pdf"
-    
+
     parser = Pdf_Parser(file_name)
-    
+
     assert file_name == parser.get_file_name()
-    
-    
