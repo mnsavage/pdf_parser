fa4a65914fc103d21955f6921437449cf6c4492f
themnsavage <mnsavage@crimson.ua.edu>
2023-10-05 15:54:52 -0500

    feat: get function that returns file name

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -1,3 +1,7 @@
+import json
 class Pdf_Parser:
     def __init__(self, file_name):
         self._file_name = file_name
+    
+    def get_file_name(self):
+        return self._file_name
--- tests/test_pdf_parser.py
+++ tests/test_pdf_parser.py
@@ -8,3 +8,12 @@ def test_init():
     parser = Pdf_Parser(file_name)
 
     assert file_name == parser._file_name
+    
+def test_get_file_name():
+    file_name = "file/path/file.pdf"
+    
+    parser = Pdf_Parser(file_name)
+    
+    assert file_name == parser.get_file_name()
+    
+    
