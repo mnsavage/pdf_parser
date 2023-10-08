201ea1b347c6e26c05426808e003ca21ea0b571c
chp2001 <chp.2001.jhp@gmail.com>
2023-10-08 15:29:00 -0500

    Added functionality to pdf_parser, created 2 tests

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -1,3 +1,64 @@
+import pdfminer
+import pdfminer.high_level as pdf_hl
+import pdfminer.layout as pdf_layout
+import os
+from src.page_parser import Page_Parser
+
+
 class Pdf_Parser:
     def __init__(self, file_name):
         self._file_name = file_name
+        self._pages = []
+        self._page_handlers = []
+        self._unpacked = False
+        self._page_count = 0
+        self._text_info = {}
+
+    def get_pages(self):
+        self.unpack()
+        return self._pages
+    
+    def get_page_handlers(self):
+        self.unpack()
+        return self._page_handlers
+    
+    def get_page_count(self):
+        self.unpack()
+        return self._page_count
+    
+    def get_unpacked(self):
+        return self._unpacked
+
+    def unpack(self):
+        if not self._unpacked:
+            self._pages = pdf_hl.extract_pages(self._file_name)
+            self._pages = list(self._pages)
+
+            page_handlers = []
+            for page in self._pages:
+                self._page_handlers.append(Page_Parser(page))
+            for ind, page in enumerate(self._page_handlers):
+                page: Page_Parser
+                page.unpack_page()
+
+            self._page_count = len(self._page_handlers)
+            self._unpacked = True
+
+    def margin_check(self, leftWidth, rightWidth):
+        self.unpack()
+        for page in self._page_handlers:
+            page: Page_Parser
+            if page.content_bbox[0] < leftWidth:
+                return False
+            if page.content_bbox[2] > page.width - rightWidth:
+                return False
+        return True
+    
+    def margin_check_inches(self, leftWidth, rightWidth):
+        self.unpack()
+        leftWidth = self._inches_to_points(leftWidth)
+        rightWidth = self._inches_to_points(rightWidth)
+        return self.margin_check(leftWidth, rightWidth)
+    
+    def _inches_to_points(self, inches):
+        return inches * 72
--- tests/test_pdf_parser.py
+++ tests/test_pdf_parser.py
@@ -1,6 +1,7 @@
 import pytest
 from src.pdf_parser import Pdf_Parser
-
+from src.page_parser import Page_Parser
+import os
 
 def test_init():
     file_name = "file/path/file.pdf"
@@ -8,3 +9,30 @@ def test_init():
     parser = Pdf_Parser(file_name)
 
     assert file_name == parser._file_name
+
+def test_unpack():
+    #Select a PDF file to test with, assert that there are pages
+    #Assert that our parser has the pages and page handlers stored
+    file_name = "Homework_1.pdf"
+    file_name_path = os.path.join(
+        os.path.dirname(__file__), "..", "prototyping", file_name
+    )
+    parser = Pdf_Parser(file_name_path)
+
+    parser.unpack()
+
+    assert len(parser._pages) > 0
+    assert len(parser._page_handlers) > 0
+    assert parser.get_unpacked() == True
+
+def test_margin_check():
+    #Select a PDF file to test with, assert that the margins are good
+    file_name = "Homework_1.pdf"
+    file_name_path = os.path.join(
+        os.path.dirname(__file__), "..", "prototyping", file_name
+    )
+    parser = Pdf_Parser(file_name_path)
+
+    parser.unpack()
+
+    assert parser.margin_check(72, 72) == True
