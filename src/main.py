03f5eeef44d3d2ca5030755836aaa2cae32e662b
themnsavage <mnsavage@crimson.ua.edu>
2023-11-04 02:33:39 -0500

    fix: fix imports to be able to be use in docker and unit tests locally

--- src/main.py
+++ src/main.py
@@ -2,16 +2,16 @@
 import argparse
 from pdf_parser import Pdf_Parser
 
+
 def main():
     arg_parser = argparse.ArgumentParser(description="Process some parameters.")
-    arg_parser.add_argument('dynamoDB', help='data base')
-    arg_parser.add_argument('UUID', help='unique id')
-    arg_parser.add_argument('pdf', help='pdf file to parse')
+    arg_parser.add_argument("dynamoDB", help="data base")
+    arg_parser.add_argument("UUID", help="unique id")
+    arg_parser.add_argument("pdf", help="pdf file to parse")
     args = arg_parser.parse_args()
 
-    
-    
     print(f"dynamoDB: {args.dynamoDB}, UUID: {args.UUID}, pdf: {args.pdf}")
+
 
 if __name__ == "__main__":
     main()
