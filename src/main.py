a68fb991ecad38c26431e6fc5afba4d49592c15f
themnsavage <mnsavage@crimson.ua.edu>
2023-11-04 16:28:12 -0500

    chor: get rid of comment

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-# app.py
 import argparse
 
 
 def main():
     arg_parser = argparse.ArgumentParser(description="Process some parameters.")
-    arg_parser.add_argument("dynamoDB", help="data base")
+    arg_parser.add_argument("dynamoDB", help="database")
     arg_parser.add_argument("UUID", help="unique id")
     arg_parser.add_argument("pdf", help="pdf file to parse")
     args = arg_parser.parse_args()
