4d7561c89c44ec5ae226e5c55337a518b28a1d1c
themnsavage <mnsavage@crimson.ua.edu>
2023-11-04 20:24:31 -0500

    chor: will get pdf from storage

--- src/main.py
+++ src/main.py
@@ -3,14 +3,11 @@ import argparse
 
 def main():
     arg_parser = argparse.ArgumentParser(description="Process some parameters.")
-    arg_parser.add_argument("dynamoDB", help="database")
+    arg_parser.add_argument("storage", help="where the encoded pdf is stored")
     arg_parser.add_argument("UUID", help="unique id")
-    arg_parser.add_argument("encoded_pdf", help="encoded_pdf that is base 64 encoded")
     args = arg_parser.parse_args()
 
-    print(
-        f"dynamoDB: {args.dynamoDB}, UUID: {args.UUID}, encoded_pdf: {args.encoded_pdf}"
-    )
+    print(f"storage: {args.storage}, UUID: {args.UUID}")
 
 
 if __name__ == "__main__":
