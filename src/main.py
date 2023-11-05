4afb58fe9e6f2a5bddabea59b2e823dc08a36932
themnsavage <mnsavage@crimson.ua.edu>
2023-11-04 20:17:47 -0500

    doc: ensure to know the pdf is encoded when pass to docker image

--- src/main.py
+++ src/main.py
@@ -5,10 +5,12 @@ def main():
     arg_parser = argparse.ArgumentParser(description="Process some parameters.")
     arg_parser.add_argument("dynamoDB", help="database")
     arg_parser.add_argument("UUID", help="unique id")
-    arg_parser.add_argument("pdf", help="pdf file to parse")
+    arg_parser.add_argument("encoded_pdf", help="encoded_pdf that is base 64 encoded")
     args = arg_parser.parse_args()
 
-    print(f"dynamoDB: {args.dynamoDB}, UUID: {args.UUID}, pdf: {args.pdf}")
+    print(
+        f"dynamoDB: {args.dynamoDB}, UUID: {args.UUID}, encoded_pdf: {args.encoded_pdf}"
+    )
 
 
 if __name__ == "__main__":
