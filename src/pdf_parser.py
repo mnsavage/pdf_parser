508e196eec93423b8cff49d67eb5b1a75c3371cf
rqualls <rachellqualls@gmail.com>
2023-11-07 17:23:07 -0600

    functionality for checking title page

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -1,11 +1,13 @@
 import pdfminer.high_level as pdf_hl
 from page_parser import Page_Parser
+import os
 
 
 class Pdf_Parser:
     def __init__(self, file_name):
         self._file_name = file_name
         self._pages = []
+        self._first_page_contents = []
         self._page_handlers = []
         self._unpacked = False
         self._page_count = 0
@@ -28,8 +30,18 @@ class Pdf_Parser:
 
     def unpack(self):
         if not self._unpacked:
+            
             self._pages = pdf_hl.extract_pages(self._file_name)
             self._pages = list(self._pages)
+
+            # custom sorting to make sure the higher y val goes first in the list
+            def sort_by_ypos(e):
+                return e.y0
+            
+            firstPage = list(self._pages[0])
+            firstPage.sort(key=sort_by_ypos, reverse=True)
+            self._first_page_contents = firstPage
+
 
             for page in self._pages:
                 self._page_handlers.append(Page_Parser(page))
@@ -198,6 +210,207 @@ class Pdf_Parser:
                 if page.is_bold(font):
                     return False
         return True
+    
+    # MARK: Check title page for thesis/dissertation
+
+    # check for the location of "by" in the title page
+    # and if it is correctly formated to be lowercase
+    # if there is no by, return -1
+    def _check_by(self):
+        self.unpack()
+
+        for index, text in enumerate(self._first_page_contents):
+            if text.get_text().strip().lower() == 'by':
+                return {'found_location': index, 'correct_format': text.get_text().strip() == 'by'}
+        return {'found_location': -1} # should throw/ display error here
+    
+    # check the spacing between lines
+    #   lines: an array of lines to check spacing between
+    #   returns: the spacing type (1, 1.5, 2, 2.5, 3)
+    #   returns -1 if lines is an array with less than 2 elements
+    #   or if the line spacing is not consistant throughout
+    def _check_line_spacing(self, lines):
+        self.unpack()
+
+        if len(lines) < 2:
+            return -1
+        spacing = 0
+        for lineNum in range(len(lines) - 1):
+            current = lines[lineNum]
+            next = lines[lineNum + 1]
+            curSize = current.y1 - current.y0
+            between = current.y0 - next.y1
+            base = 0.15 * curSize
+            difference = 0.58 * curSize 
+            # spacing of 1
+            if between > base - 1 and between < base + 1:
+                if spacing == 0:
+                    spacing = 1
+                elif spacing != 1:
+                    return -1
+            # spacing of 1.5
+            elif between > base + difference - 1 and between < base + difference + 1:
+                if spacing == 0:
+                    spacing = 1.5
+                elif spacing != 1.5:
+                    return -1
+            # spacing of 2
+            elif between > base + 2 * difference - 1 and between < base + 2 * difference + 1:
+                if spacing == 0:
+                    spacing = 2
+                elif spacing != 2:
+                    return -1
+            # spacing of 2.5
+            elif between > base + 3 * difference - 1 and between < base + 3 * difference + 1:
+                if spacing == 0:
+                    spacing = 2.5
+                elif spacing != 2.5:
+                    return -1
+            # spacing of 3
+            elif between > base + 4 * difference - 1 and between < base + 4 * difference + 1:
+                if spacing == 0:
+                    spacing = 3
+                elif spacing != 3:
+                    return -1
+            else: return -1
+        return spacing
+
+    # Title must be at least three lines that are made into an inverted pyramid and double spaced
+    def check_title_format(self):
+        self.unpack()
+
+        byInfo = self._check_by()
+        if byInfo['found_location'] == -1:
+            return False
+        
+        lineArray = []
+        lineWidth = 0
+        for text in self._first_page_contents[:byInfo['found_location']]:
+            for line in text:
+                print(line)
+                print(lineArray)
+                if line.get_text().strip() == "":
+                    if len(lineArray) > 0:
+                        break
+                else:
+                    if lineWidth == 0:
+                        lineWidth = line.x1 = line.x0
+                    else:
+                        newLineWidth = line.x1 - line.x0
+                        if newLineWidth > lineWidth:
+                            return False
+                        lineWidth = newLineWidth
+                    lineArray.append(line)
+        return self._check_line_spacing(lineArray) == 2 and len(lineArray) == 3
+
+    # 2 double spaces beneath title
+    def check_spacing_beneath_title(self):
+        self.unpack()
+
+        byInfo = self._check_by()
+        if byInfo['found_location'] == -1:
+            return False
+        
+        lineArray = []
+        for text in reversed(self._first_page_contents[:byInfo['found_location']]):
+            for line in text:
+                if line.get_text().strip() == "":
+                    lineArray.insert(0, line)
+                else:
+                    break
+        print()
+        return self._check_line_spacing(lineArray) == 2 and len(lineArray) == 2
+
+    # "by" is in all lowercase in the title page
+    def check_by_lowercase(self):
+        self.unpack()
+
+        byInfo = self._check_by()
+        return byInfo['found_location'] != -1 and byInfo['correct_format']
+
+    # There cannot be two co-chairs; one must be a chair and one is a co-chair
+    # Checks for one appearence of "chair" and not more than one appearence of "co-chair"
+    def check_chair_requirement(self):
+        self.unpack()
+
+        numChairs = 0
+        numCoChairs = 0
+        for text in self._first_page_contents:
+            numChairs += text.get_text().strip().lower().count("committee chair")
+            numCoChairs += text.get_text().strip().lower().count("co-chair")
+        return numChairs == 1 and numCoChairs <= 1
+
+    # Correct department listed
+    # Checks for "the Department of"
+    def check_department(self):
+        self.unpack()
+
+        for text in self._first_page_contents:
+            if "the department of" in text.get_text().strip().lower():
+                return True
+        return False
+
+    # TUSCALOOSA, ALABAMA in all caps with ALABAMA spelled out
+    def check_location_requirement(self):
+        self.unpack()
+
+        for text in self._first_page_contents:
+            if text.get_text().strip() == 'TUSCALOOSA, ALABAMA':
+                return True
+        return False
+
+    # Year of graduation, not year of submission, at bottom of the page
+    # Checks for year at the bottom of page
+    def check_graduation_year(self):
+        self.unpack()
+
+        for text in reversed(self._first_page_contents):
+            if text.get_text().strip() != '':
+                return text.get_text().strip().isdigit()
+        return False
+
+    # Returns student fname and lname
+    # if 'by' or the name is not found, return false
+    def get_student_name(self):
+        self.unpack()
+
+        byInfo = self._check_by()
+
+        if byInfo['found_location'] != -1:
+            for text in self._first_page_contents[byInfo['found_location'] + 1:]:
+                if text.get_text().strip() != '':
+                    name = text.get_text().strip().split()
+                    lname = name[len(name) - 1]
+                    fname = ' '.join(name[:len(name) - 1])
+                    return {'fname': fname, 'lname': lname}
+        return False
+            
+
+    # Returns if document is a thesis or dissertation
+    # if neiter is found, return false
+    def get_paper_type(self):
+        self.unpack()
+
+        for text in self._first_page_contents:
+            if 'dissertation' in text.get_text().strip().lower():
+                return 'dissertation'
+            if 'thesis' in text.get_text().strip().lower():
+                return 'thesis'
+        return False
+
 
     def get_file_name(self):
         return self._file_name
+
+
+def main():
+    file_name = "correct_title.pdf"
+    file_name_path = os.path.join(
+        os.path.dirname(__file__), "..", "prototyping", file_name
+    )
+    parser = Pdf_Parser(file_name_path)
+
+    print(parser.check_title_format())
+
+if __name__ == "__main__":
+    main()
