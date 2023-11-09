8d141f0fcb648aebbe842f7d52a425ad4b5df6b8
themnsavage <mnsavage@crimson.ua.edu>
2023-11-08 19:34:50 -0600

    feat: use enviroment variables and update output and status in db

--- src/main.py
+++ src/main.py
@@ -1,13 +1,50 @@
-import argparse
-
+import os
+import boto3
 
 def main():
-    arg_parser = argparse.ArgumentParser(description="Process some parameters.")
-    arg_parser.add_argument("storage", help="where the encoded pdf is stored")
-    arg_parser.add_argument("UUID", help="unique id")
-    args = arg_parser.parse_args()
+    storage_name = os.getenv('DYNAMODB_NAME', None)
+    UUID = os.getenv('DYNAMODB_KEY', None)
+    
+    # check if environment variables were retrieve
+    if storage_name is None and UUID is None:
+        raise ValueError('Environment variables were not retrieve')
+    
+    try:
+        # Create a DynamoDB client
+        dynamodb = boto3.resource('dynamodb')
 
-    print(f"storage: {args.storage}, UUID: {args.UUID}")
+        # Reference the DynamoDB table
+        table = dynamodb.Table(storage_name)
+
+        # Key of the item you want to update (replace UUID with your actual uuid value)
+        key = {'uuid': UUID}
+
+        # Define new values for job_status and job_output
+        new_job_status = 'completed'  
+        new_job_output = 'hello world from batch job'
+
+        # Update the 'job_status' and 'job_output' attributes of the item with the given key
+        update_expression = "SET job_status = :new_status, job_output = :new_output"
+        expression_attribute_values = {
+            ':new_status': new_job_status,
+            ':new_output': new_job_output,
+        }
+
+        # Perform the update operation
+        response = table.update_item(
+            Key=key,
+            UpdateExpression=update_expression,
+            ExpressionAttributeValues=expression_attribute_values,
+            ReturnValues="UPDATED_NEW"  # Returns the new values of the updated attributes
+        )
+
+        print(response)
+        
+    except ClientError as e:
+        print(f"Error: {e.response['Error']['Message']}")
+        
+    else:
+        print('Updating database was successful')
 
 
 if __name__ == "__main__":
