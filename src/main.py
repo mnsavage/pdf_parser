62887f89a431eaa500526977483c50f5268b6c9e
themnsavage <mnsavage@crimson.ua.edu>
2023-11-08 19:39:05 -0600

    refactor: fix linting

--- src/main.py
+++ src/main.py
@@ -1,33 +1,35 @@
 import os
 import boto3
+from botocore.exceptions import ClientError
+
 
 def main():
-    storage_name = os.getenv('DYNAMODB_NAME', None)
-    UUID = os.getenv('DYNAMODB_KEY', None)
-    
+    storage_name = os.getenv("DYNAMODB_NAME", None)
+    UUID = os.getenv("DYNAMODB_KEY", None)
+
     # check if environment variables were retrieve
     if storage_name is None and UUID is None:
-        raise ValueError('Environment variables were not retrieve')
-    
+        raise ValueError("Environment variables were not retrieve")
+
     try:
         # Create a DynamoDB client
-        dynamodb = boto3.resource('dynamodb')
+        dynamodb = boto3.resource("dynamodb")
 
         # Reference the DynamoDB table
         table = dynamodb.Table(storage_name)
 
         # Key of the item you want to update (replace UUID with your actual uuid value)
-        key = {'uuid': UUID}
+        key = {"uuid": UUID}
 
         # Define new values for job_status and job_output
-        new_job_status = 'completed'  
-        new_job_output = 'hello world from batch job'
+        new_job_status = "completed"
+        new_job_output = "hello world from batch job"
 
         # Update the 'job_status' and 'job_output' attributes of the item with the given key
         update_expression = "SET job_status = :new_status, job_output = :new_output"
         expression_attribute_values = {
-            ':new_status': new_job_status,
-            ':new_output': new_job_output,
+            ":new_status": new_job_status,
+            ":new_output": new_job_output,
         }
 
         # Perform the update operation
@@ -35,16 +37,16 @@ def main():
             Key=key,
             UpdateExpression=update_expression,
             ExpressionAttributeValues=expression_attribute_values,
-            ReturnValues="UPDATED_NEW"  # Returns the new values of the updated attributes
+            ReturnValues="UPDATED_NEW",  # Returns the new values of the updated attributes
         )
 
         print(response)
-        
+
     except ClientError as e:
         print(f"Error: {e.response['Error']['Message']}")
-        
+
     else:
-        print('Updating database was successful')
+        print("Updating database was successful")
 
 
 if __name__ == "__main__":
