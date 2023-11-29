6e79b861bf549617c036b6ce38396a2cea0a2b18
themnsavage <mnsavage@crimson.ua.edu>
2023-11-29 01:12:18 -0600

    fix: logic was reverse on what to do if validation failed or succesed

--- src/main.py
+++ src/main.py
@@ -6,26 +6,75 @@ from io import BytesIO
 from botocore.exceptions import ClientError
 from pdf_parser import Pdf_Parser
 
+def get_file_info(pdf):
+    parser = Pdf_Parser(pdf)
+    first_name, last_name = parser.get_student_name()
+    first_name = "could_not_find_name" if first_name is None else first_name
+    last_name = "error" if last_name is None else last_name
+    paper_type = parser.get_paper_type()
+    paper_type = "could_not_find_paper_type" if paper_type is None else paper_type
+    new_file_name = f'{last_name}.{first_name}.{paper_type}'
 
+
+    return first_name, last_name, new_file_name
+
+def handle_pdf_requirements_validation_failure(e, encoded_pdf, file_name, table, key):
+        error_message = f"Error message: {e}"
+        
+        pdf_io = convert_encoded_pdf_to_io(encoded_pdf=encoded_pdf)
+        first_name, last_name, new_file_name = get_file_info(pdf_io)
+        with open('pdf_validation_output.json', 'r') as file:
+            data = json.load(file)
+            
+        #file info
+        data["name"] = file_name
+        data["newName"] = new_file_name
+        data["fname"] = first_name
+        data["lname"] = last_name
+        
+        update_expression = "SET job_status = :new_status, job_output = :new_output"
+        expression_attribute_values = {
+            ":new_status": e,
+            ":new_output": json.dumps(data),
+        }
+        table.update_item(
+            Key=key,
+            UpdateExpression=update_expression,
+            ExpressionAttributeValues=expression_attribute_values,
+            ReturnValues="UPDATED_NEW",  # Returns the new values of the updated attributes
+        )
+        raise Exception(error_message)
+    
+def upload_successful_pdf_requirements_validation(job_output, table, key):
+        update_expression = "SET job_status = :new_status, job_output = :new_output"
+        expression_attribute_values = {
+            ":new_status": "completed",
+            ":new_output": json.dumps(job_output),
+        }
+
+        table.update_item(
+            Key=key,
+            UpdateExpression=update_expression,
+            ExpressionAttributeValues=expression_attribute_values,
+            ReturnValues="UPDATED_NEW",  # Returns the new values of the updated attributes
+        )
+    
 def get_pdf_requirements_validation(pdf, old_file_name):
     """
     Description: creates a dictionary with information about the requirements the pdf passes or not
     """
     parser = Pdf_Parser(pdf)
+    first_name, last_name, new_file_name = get_file_info(pdf)
 
-    first_name, last_name = parser.get_student_name()
-    first_name = "could_not_find_name" if first_name is None else first_name
-    last_name = "error" if last_name is None else last_name
 
-    paper_type = parser.get_paper_type()
-    paper_type = "could_not_find_paper_type" if paper_type is None else paper_type
+
     
     with open('pdf_validation_output.json', 'r') as file:
         data = json.load(file)
         
     #file info
     data["name"] = old_file_name
-    data["newName"] = f'{last_name}.{first_name}.{paper_type}'
+    data["newName"] = new_file_name
     data["fname"] = first_name
     data["lname"] = last_name
     # Page Formatting & Font
@@ -109,37 +158,10 @@ def main():
         )
 
     except ClientError as e: # encoded pdf validation fails
-        error_message = f"Error message: {e}"
-        
-        update_expression = "SET job_status = :new_status, job_output = :new_output"
-        expression_attribute_values = {
-            ":new_status": e,
-            ":new_output": json.dumps(job_output),
-        }
-        table.update_item(
-            Key=key,
-            UpdateExpression=update_expression,
-            ExpressionAttributeValues=expression_attribute_values,
-            ReturnValues="UPDATED_NEW",  # Returns the new values of the updated attributes
-        )
-        raise Exception(error_message)
+        handle_pdf_requirements_validation_failure(e, encoded_pdf, file_name, table)
 
     else:
-        with open('pdf_validation_output.json', 'r') as file:
-            data = json.load(file)
-        
-        update_expression = "SET job_status = :new_status, job_output = :new_output"
-        expression_attribute_values = {
-            ":new_status": "completed",
-            ":new_output": json.dumps(data),
-        }
-
-        table.update_item(
-            Key=key,
-            UpdateExpression=update_expression,
-            ExpressionAttributeValues=expression_attribute_values,
-            ReturnValues="UPDATED_NEW",  # Returns the new values of the updated attributes
-        )
+        upload_successful_pdf_requirements_validation(job_output, table, key)
 
 
 if __name__ == "__main__":
